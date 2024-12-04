bec670628d9006e71cb0f999bdf041cc1976362c
Oscar Devs <ohernandezba@gmail.com>
2024-12-04 22:48:23 +0100

    Salas aleatorias

--- Unity3d/PreMultiplayer_PUN2/Assets/Scripts/NetworkingManager.cs
+++ Unity3d/PreMultiplayer_PUN2/Assets/Scripts/NetworkingManager.cs
@@ -1,12 +1,45 @@
 using Photon.Pun;
+using Photon.Realtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class NetworkingManager : MonoBehaviourPunCallbacks
 {
     public Button multiplayerButton;
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Unirnos a un lobby");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Estamos listo para multijugador");
+        multiplayerButton.interactable = true;
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No se pudo encontrar una sala");
+        CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Cargando escena de juego");
+        SceneManager.LoadScene(1);
+    }
+
+    public void FindMatch()
+    {
+        Debug.Log("Buscando una sala");
+        PhotonNetwork.JoinRandomRoom();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +54,19 @@ public class NetworkingManager : MonoBehaviourPunCallbacks
 
     }
 
-    public override void OnConnectedToMaster()
+    private void CreateRoom()
     {
-        Debug.Log("Unirnos a un lobby");
-        PhotonNetwork.JoinLobby();
-    }
+        int randomRoomName = UnityEngine.Random.Range(0, 10000);
 
-    public override void OnJoinedLobby()
-    {
-        Debug.Log("Estamos listo para multijugador");
-        multiplayerButton.interactable = true;
+        RoomOptions roomOptions = new RoomOptions
+        {
+            IsVisible = true,
+            IsOpen = true,
+            MaxPlayers = 6,
+            PublishUserId = true
+        };
+
+        PhotonNetwork.CreateRoom($"RoomName_{randomRoomName}", roomOptions);
+        Debug.Log($"Sala creada: {randomRoomName}");
     }
 }
