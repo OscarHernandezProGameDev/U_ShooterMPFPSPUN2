29fb7a520d12287dadec9c94962a48d4d19b8bcd
Oscar Devs <ohernandezba@gmail.com>
2024-12-04 22:21:27 +0100

    El Networking Manager

---
+++ Unity3d/PreMultiplayer_PUN2/Assets/Scripts/NetworkingManager.cs
@@ -0,0 +1,31 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkingManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Conexión a un servidor");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Unirnos a un lobby");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Estamos listo para multijugador");
+    }
+}
